70f7e5646fa6888e4ee805625e0d13071ad37612
vitaly <50383391+Neikow@users.noreply.github.com>
2025-10-24 22:59:08 +0200

    fix compilation

--- frontend/src/hooks/useRoomPlayers.tsx
+++ frontend/src/hooks/useRoomPlayers.tsx
@@ -1,22 +1,28 @@
+import type { Player } from '../types/player.ts'
 import { useEffect, useMemo } from 'react'
 import { useGameRoom } from '../providers/GameRoomProvider.tsx'
 import { usePlayersStore } from '../stores/usePlayersStore.tsx'
 
 export function useRoomPlayers() {
-  const { client } = useGameRoom()
+  const { client, roomId } = useGameRoom()
   const { players, setPlayers } = usePlayersStore()
 
   useEffect(() => {
     return client.on((message) => {
       if (message.type === 'snapshot') {
-        setPlayers(message.data.players)
+        if (message.data.players)
+          setPlayers(message.data.players.map(player => ({ ...player, room_id: roomId })))
       }
       else if (message.type === 'event') {
         if (message.event.type === 'player.joined') {
-          const player = message.event.data
+          const player = message.event.data as Player
           setPlayers(players => [...players, { ...player, status: 'connected' }])
         }
         else if (message.event.type === 'player.left') {
+          if (!message.event.data) {
+            console.error('player.left event missing data')
+            return
+          }
           const id = message.event.data.id
           setPlayers((players) => {
             return players.map((player) => {
